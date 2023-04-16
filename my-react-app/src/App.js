8eff3f575e29a212b6c064407b19d76f410c045f
Anoushka Makkad <amakkad@usc.edu>
2023-04-15 19:40:29 -0700

    login

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,8 @@ import {AddTask} from "./pages/AddTask";
 import { Route, Link } from "react-router-dom";
 import { Routes } from 'react-router-dom';
 import { ScheduleCloseup } from "./pages/ScheduleCloseup";
+import { Login} from "./pages/Login";
+import { Register} from "./pages/Register";
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
        <Route path="/AddTask" element={<AddTask />}/>
       <Route path="/AddTask" element={<AddTask />}/>
       <Route path="/ScheduleCloseup" element={<ScheduleCloseup />}/>
+      <Route path="/Login" element={<Login />}/>
+      <Route path="/Register" element={<Register />}/>
       </Routes>
     </div>
   );
