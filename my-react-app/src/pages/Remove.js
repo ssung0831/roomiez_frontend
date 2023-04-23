ef64d82a0b425557798d5dbeadcfbe889b51215d
Stephanie Yu <sayu@usc.edu>
2023-04-22 17:49:57 -0700

    stph remove update

--- my-react-app/src/pages/Remove.js
+++ my-react-app/src/pages/Remove.js
@@ -5,38 +5,106 @@ import myImage from './circle.png';
 
 export const Remove = () => {
 
+    var loggedIn = false;
+    var userId = "-1";
+    const [removeSuccess, setRemoveSuccess] = useState(false);
+
+    //read in cookie on open
+    window.onload = function getID() {
+        //check if user is logged in
+      const name = 'userId';
+      console.log(document.cookie);
+
+      console.log(getCookie(name));
+
+      const newId = getCookie(name)
+      if (getCookie(name) !== ""){
+        loggedIn = true;
+        userId = newId;
+        //call function to create navbar
+        //createNav();
+      }
+      //track cookie
+      console.log(loggedIn);
+      console.log(userId);
+    }
+
+    //read cookieValue
+    function getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+
     function Box({ children, ...props }) {
         return <div {...props}>{children}</div>
     }
 
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const[assignTo, setAssignTo] = useState('');
-    const [repeat, setRepeat] = useState('daily');
+    const [username, setName] = useState('');
     const [userID, setUserID] = useState(null);
+    //http://localhost:8080/user/${userID}/setGroup?groupID=0
 
+    const handleSubmit = (event) =>{
+      event.preventDefault();
+      // Retrieve the username from the state
+      const username = document.getElementById("text").value;
+      console.log("Submitted username:", username);
+      getUserID();
+
+    }
 
     function getUserID() {
-      fetch(`localhost:8080/user/${name}`, {
+      console.log(username);
+      fetch(`http://roomieztestnv-env.eba-s98dmkpn.us-east-1.elasticbeanstalk.com/user/getId/${username}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
-      });
-      const data = Response.json();
-      console.log(data);
-      setUserID(data);
-    }
-  
-    const handleSubmit = (event) => {
-      getUserID();
-      event.preventDefault();
-      // console.log(event);
-      fetch('/Remove', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name: name})
-      });
-    }
+      })
+      .then(response => response.json())
+      .then(data => {
+        //data = Response.json();
+        //var userid = data; 
+        console.log("this is data:"+ data);
+        setUserID(data);
 
+        // fetch(`http://localhost:8080/user/${userID}/setGroup?groupID=3`, { mode: 'no-cors' })
+        // .then(response => {
+        //   // Handle response
+
+        // })
+        fetch(`http://roomieztestnv-env.eba-s98dmkpn.us-east-1.elasticbeanstalk.com/user/${data}/setGroup?groupID=0`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          })
+        .then(response => {
+          //console.log(response);
+          if (response.status===200) {
+            // group created successfully
+            console.log("roomie removed successfully:", response.json());
+            setRemoveSuccess(true);
+            // return response.json();
+          } else {
+            // handle error
+            console.log("roomie removal failed:", response.status);
+            throw new Error('roomie removal failed');
+          }
+    })
+  
+      });
+     
+    }
+    
 
   return (
     
@@ -51,17 +119,17 @@ export const Remove = () => {
    
     <div className="loginBox">
     <div style={{ textAlign: 'center', fontSize: '40px', color: '#934C81', fontWeight: 'bold', marginTop: '2%' }}>Remove Roomate</div>
+    {removeSuccess && <p>Remove Successful</p>}
+    
+    <form className="addTaskForm" onSubmit = {handleSubmit}>
+
+      <div className="question form-group">
+
+        <input id="text" value={username} onChange={(event) => setName(event.target.value)} type="username" placeholder = "Roomate email" required></input>
+      </div>
 
 
-    <form class="addTaskForm" onSubmit = {handleSubmit}>
-
-      <div class="question form-group">
-  {/* <label for="name"> task name: </label> */}
-  <input id="text" value={name} onChange={(event) => setName(event.target.value)} type="name" placeholder = "Roomate name" required></input>
-</div>
-
-
-        <button type="submit" class="btn3">Submit</button>
+        <button type="submit" className="btn3">Submit</button>
      </form>
      
      </div>
